a647c691258e31b2529783a64dbd25abf6f0b2d2
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-07-25 22:54:20 +0600

    and here is the today's leetcode make sure to try you first

---
+++ Codes/leetcode/387.First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> m;
+        queue<int> q;
+
+        for(int i=0; i<s.size(); i++){
+            if(.find(s[i]) == m.end()) {
+                q.push(i);
+            }
+
+            m[s[i]]++:
+
+            while ((q.size() > 0 && m[s[q.front()]] > 1))
+            {
+                q.pop();
+            }
+            
+        }
+        return q.empty() ? -1 : q.front();
+    }
+};

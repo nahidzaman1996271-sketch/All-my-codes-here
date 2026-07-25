b7de3b8afdfc4254d71cc8449e0108058fee4037
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-07-25 21:15:48 +0600

    this is the new code of the file handling problems and i tried it out and it is so good

--- Codes/File handling/storing student details in file handling/demo8.c
+++ Codes/File handling/storing student details in file handling/demo8.c
@@ -1,36 +1,52 @@
-#include<stdio.h>
-int main()
-{
+#include <stdio.h>
+#include <string.h>
+
+int main() {
     FILE *file;
     char name[20];
-    int age,num,i=0;
+    int age, num, i;
     long phoneNumber;
 
-    file = fopen("student.txt","a"); // a means append
+    file = fopen("student.txt", "a");
 
-    if(file==NULL){
-        printf("File doesn't exist");
+    if (file == NULL) {
+        printf("File doesn't exist\n");
+        return 1;
     }
-    else{
-        printf("File is opened\n");
 
-        printf("Enter number of students: ");
-        scanf("%d",&num);
+    printf("File is opened\n");
 
-        for(i=1;i<=num;i++){
-            printf("Enter student name: ");
-            scanf("%s",name);
-
-            printf("Enter student age: ");
-            scanf("%d",&age);
-
-            printf("Enter student phone number: ");
-            scanf("%ld",&phoneNumber);
-
-            fprintf(file,"%-15s%-8d%-15ld\n",name,age,phoneNumber);
-        }
+    printf("Enter number of students: ");
+    if (scanf("%d", &num) != 1 || num <= 0) {
+        printf("Invalid number of students.\n");
         fclose(file);
+        return 1;
     }
+    while (getchar() != '\n');
+
+    for (i = 1; i <= num; i++) {
+        printf("Enter student name: ");
+        fgets(name, sizeof(name), stdin);
+        name[strcspn(name, "\n")] = '\0';
+
+        printf("Enter student age: ");
+        while (scanf("%d", &age) != 1) {
+            printf("Invalid input. Enter student age: ");
+            while (getchar() != '\n');
+        }
+
+        printf("Enter student phone number: ");
+        while (scanf("%ld", &phoneNumber) != 1) {
+            printf("Invalid input. Enter student phone number: ");
+            while (getchar() != '\n');
+        }
+        while (getchar() != '\n');
+
+        fprintf(file, "%-15s%-8d%-15ld\n", name, age, phoneNumber);
+    }
+
+    fclose(file);
+    printf("Data saved successfully.\n");
 
     return 0;
 }

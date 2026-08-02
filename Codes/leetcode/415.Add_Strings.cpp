80f2e811c48a089f6bf813bb62ab226d43aa2750
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-08-02 21:40:55 +0600

    and this is the solve of today's code i mean 08/02/26

--- Codes/leetcode/415.Add_Strings.cpp
+++ Codes/leetcode/415.Add_Strings.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        
+        string ans;
+        int carry=0;
+        int i=num1.size();
+        int j=num2.size();
+        while(i>=0 || j>=0 || carry){
+            if(i>=0){
+                carry+=num1[i--]-'0';
+            }
+            if(j>=0){
+                carry+=num2[j--]-'0';
+            }
+            ans+=carry%10 + -'0';
+            carry/=10;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };

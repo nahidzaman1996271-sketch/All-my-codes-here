ba92a9dffd0e5bc89c7bea601712b9d6a0da4af7
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-08-03 22:04:30 +0600

    and this is the today's leetcode and this is impressive

--- Codes/leetcode/434.Number_of_Segments_in_a_String.cpp
+++ Codes/leetcode/434.Number_of_Segments_in_a_String.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int countSegments(string s) {
-        
+        int count = 0;
+        bool isSegment = false;
+
+        for(int i=0; i<s.length(); i++){
+            if(s[i]!=' '){
+                if(!isSegment){
+                    count++;
+                    isSegment = true;
+                }
+            }else{
+                isSegment = false;
+            }
+        }
+        return count;
     }
 };

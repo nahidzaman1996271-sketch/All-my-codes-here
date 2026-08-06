b209242826a20557d88a0071b506aa0d94bdf27e
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-08-06 21:52:02 +0600

    and this is the code of the leetcode that we solve today

--- Codes/leetcode/448.Find_All_Numbers_Disappeared_in_an_Array.cpp
+++ Codes/leetcode/448.Find_All_Numbers_Disappeared_in_an_Array.cpp
@@ -2,5 +2,16 @@ class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         
+        unordered_set<int> set(nums.begin(),nums.end());
+
+        vector<int> result;
+
+        for(int i=1;i<=nums.size();i++){
+            
+            if(set.find(i) == set.end()){
+                result.push_back(i);
+            }
+        }
+        return result;
     }
 };

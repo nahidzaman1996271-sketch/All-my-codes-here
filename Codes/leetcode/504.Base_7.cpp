360a45a6f1f13477bfbbba507508e6f05811b800
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-08-21 00:04:23 +0600

    so this is the today's leetcode problem and here is the solve and we learnt a new topic today

--- Codes/leetcode/504.Base_7.cpp
+++ Codes/leetcode/504.Base_7.cpp
@@ -2,5 +2,19 @@ class Solution {
 public:
     string convertToBase7(int num) {
         
+        if(num == 0) return "0";
+        bool isNeg = num < 0 ? true : false;
+        num = abs(num);
+        string out = "";
+        while(num > 0){
+            int r = num % 7;
+            out += to_string(r);
+            num /= 7;
+        }
+        reverse(out.begin(),out.end());
+        if(isNeg){
+            out = "-" + out;
+        }
+        return out;
     }
 };
